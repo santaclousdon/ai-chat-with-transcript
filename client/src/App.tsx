139125320d82c8360d5696485512480f0000c778
John Romas <john.romas87@gmail.com>
2025-05-19 16:10:44 -0400

    add chakra ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
+import { Button, HStack } from '@chakra-ui/react'
 
 function App() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     fetch(import.meta.env.VITE_API_URL + '/')
       .then((res) => res.json())
-      .then(setUsers);
-  }, []);
+      .then(setUsers)
+  }, [])
 
   return (
     <div>
       <h1>Users</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
